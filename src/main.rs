ee9dc247dccb63d2d234be671d235e87b058e12a
charlesndalton <charlesndalton@gmail.com>
2022-05-16 15:39:33 -0700

    feat: complete daily update bot core logic

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,34 @@
 use types::Result;
 use std::env;
+use std::time::{Duration};
+use std::thread;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let telegram_token = env::var("DIK_DIK_TELEGRAM_TOKEN").expect("DIK_DIK_TELEGRAM_TOKEN not set");
     let infura_api_key = env::var("INFURA_API_KEY").expect("INFURA_API_KEY not set");
 
-    let report = report_creator::create_report(&infura_api_key).await?;
-
-    println!("report: {:?}", report);
+    daily_check(&infura_api_key, &telegram_token).await?;
 
     Ok(())
 }
-    
 
-//    let mut committee_report = String::from("[DAILY TOKEMAK REPORT ☢️]:
-//");
-//
-//    for (asset, strategy_address) in eth_addresses["strategies"]["tokemak"].as_object().unwrap().iter() {
-//        let t_asset_balance = blockchain_client::get_t_asset_balance(&client, &strategy_address.as_str().unwrap()).await?;
-//        let want_balance_in_pool = blockchain_client::get_liquid_want_in_pool(&client, &strategy_address.as_str().unwrap()).await?;
-//        let want_balance_in_manager = blockchain_client::get_liquid_want_in_manager(&client, &strategy_address.as_str().unwrap()).await?;
-//        let tokemak_liquid_assets = want_balance_in_pool + want_balance_in_manager;
-//        let want_balance_in_manager_uni_lp_tokens = blockchain_client::get_want_in_univ2_pools(&client, &strategy_address.as_str().unwrap()).await?;
-//        let want_balance_in_manager_curve_lp_tokens = blockchain_client::get_want_in_curve_pools(&client, &strategy_address.as_str().unwrap()).await?;
-//        let tokemak_all_assets = tokemak_liquid_assets + want_balance_in_manager_uni_lp_tokens + want_balance_in_manager_curve_lp_tokens;
-//        let t_asset_total_supply = blockchain_client::get_t_asset_total_supply(&client, &strategy_address.as_str().unwrap()).await?;
-//        let mut health_ratio = tokemak_all_assets / t_asset_total_supply;
-//        health_ratio.rescale(2);
+async fn daily_check(infura_api_key: &str, telegram_token: &str) -> Result<()> {
+    loop {
+        let report = report_creator::create_report(&infura_api_key).await?;
 
-//        committee_report.push_str(&format!(
-//"Report for {}
-// - amount of t{} we own: {}
-// - health ratio (A/L): {}
-// - tokemak free want: {}
-// - tokemak uni LP want: {}
-// - tokemak curve LP want: {}
-// - tokemak total want: {}
-// - tokemak liabilities: {}
-//", asset, asset, t_asset_balance, health_ratio, tokemak_liquid_assets, want_balance_in_manager_uni_lp_tokens, want_balance_in_manager_curve_lp_tokens, tokemak_all_assets, t_asset_total_supply));
-//    }
+        for individual_asset_report in report {
+            let mut health_ratio = individual_asset_report.total_assets() / individual_asset_report.t_asset_total_supply();
+            health_ratio.rescale(3);
 
-//    print!("{}", committee_report);
-    //telegram_client::send_message_to_committee(&committee_report, &telegram_token).await?;
+            let message = format!("{} health ratio: {}", individual_asset_report.asset_name(), health_ratio);
+            println!("{}", message);
+            println!("{:?}", individual_asset_report);
+            telegram_client::send_message_to_committee(&message, telegram_token).await?;
+        }
 
-//    Ok(())
-//}
-
-
-async fn daily_check() -> Result<()> {
-    Ok(())
+        thread::sleep(Duration::from_secs(60 * 60 * 24));
+    }
 }
 
 mod report_creator {
@@ -212,22 +191,6 @@ mod blockchain_client {
 
         Ok(total_supply)
     }
-
-        
-    pub async fn get_liquid_want_in_pool(client: &Client, strategy_address: &str) -> Result<Decimal> {
-        let strategy_address = strategy_address.parse::<Address>()?;
-        let t_asset_address = get_t_asset_address(client, strategy_address).await?;
-
-        let want_address = TokemakStrategy::new(strategy_address, Arc::clone(&client)).want().call().await?;
-        let want = IERC20::new(want_address, Arc::clone(&client));
-
-        let want_decimals = want.decimals().call().await?.into();
-        let mut want_pool_balance = Decimal::from_i128_with_scale(want.balance_of(t_asset_address).call().await?.as_u128().try_into().unwrap(), want_decimals); // t asset address is same as liquidity pool address
-        want_pool_balance.rescale(6);
-
-        Ok(want_pool_balance)
-    }
-
 
     pub async fn get_want_in_univ2_pools(want: &IERC20, client: &Client, accounts_to_check: &Vec<Address>) -> Result<Decimal> {
         let univ2_pool_addresses = vec!["0x61eB53ee427aB4E007d78A9134AaCb3101A2DC23", "0x470e8de2eBaef52014A47Cb5E6aF86884947F08c", "0x43AE24960e5534731Fc831386c07755A2dc33D47", "0xecBa967D84fCF0405F6b32Bc45F4d36BfDBB2E81", "0xe55c3e83852429334A986B265d03b879a3d188Ac", "0xdC08159A6C82611aEB347BA897d82AC1b80D9419", "0xAd5B1a6ABc1C9598C044cea295488433a3499eFc"];
